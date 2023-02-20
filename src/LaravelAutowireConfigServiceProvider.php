a42e5b7cd98d92ed4130914bb923904bd1113cb9
Melchior <melchiorkokernoot@gmail.com>
2023-02-20 15:54:52 +0100

    remove laravel-package-tools

--- src/LaravelAutowireConfigServiceProvider.php
+++ src/LaravelAutowireConfigServiceProvider.php
@@ -4,22 +4,15 @@ declare(strict_types=1);
 
 namespace MelchiorKokernoot\LaravelAutowireConfig;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class LaravelAutowireConfigServiceProvider extends PackageServiceProvider
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAutowireConfigServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        parent::boot();
-
         /** @var LaravelAutowireConfig $autoWirer */
         $autoWirer = $this->app->get(LaravelAutowireConfig::class);
         $this->app->afterResolving($autoWirer->getAfterResolvingClosure());
-    }
-
-    public function configurePackage(Package $package): void
-    {
-        $package->name('laravel-autowire-config');
     }
 }
